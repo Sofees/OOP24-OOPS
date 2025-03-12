f5811f09a38f4f1e383f3f5adb7f27cba27aae29
Sofia <sofia.ricupero@studio.unibo.it>
2025-03-12 18:54:29 +0100

    Update Entity.java
    Codice Entity

---
+++ src/main/java/it/unibo/oops/model/Entity.java
@@ -0,0 +1,19 @@
+package it.unibo.oops.model;
+
+public abstract class Entity {
+    protected double x, y;         // Posizione
+    protected int health;          // Salute
+    protected double speed;        // Velocità
+    protected boolean isAlive;     // Stato
+
+
+    public Entity(double x, double y, int health, double speed) {
+        this.x = x;
+        this.y = y;
+        this.health = health;
+        this.speed = speed;
+        this.isAlive = true;
+    }
+
+    public abstract void draw(Graphics g);
+}
